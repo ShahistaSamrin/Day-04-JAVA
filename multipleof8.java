49373cd43ed92ede2dfdb6d40d304965dbae4207
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-05 13:28:46 +0530

    Add files via upload

---
+++ multipleof8.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class multipleof8{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int n=s.nextInt();
+            if((n&7)==0){
+            System.out.println("multiple of 8");
+            }else{
+            System.out.println("not multiple of 8");
+            }
+        }
+    }
+}
