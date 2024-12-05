ab573f06b368f958c3407c0fe31bcce8915a7a96
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-05 15:26:02 +0530

    Add files via upload

---
+++ checkingdivisors.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class checkingdivisors{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int c=0;
+            for(int i=1;i*i<=n;i++){
+                if(n%i==0)
+                {
+                    c+=1;
+                    if(i*i!=n)
+                    {
+                        c+=1;
+                    }
+                }
+            }
+            System.out.println(c);
+    }
+}
