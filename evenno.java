abe28dff83f7a0d825631699943102176b05369d
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-05 13:28:32 +0530

    Add files via upload

---
+++ evenno.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class evenno{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        for(int i=0;i<=t;i+=2)
+        {
+            System.out.println(i);
+        }
+    }
+}
