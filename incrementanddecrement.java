a5bc71a36a6a6760f30092b4e5b44334cc1f1d57
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-05 16:03:43 +0530

    Add files via upload

---
+++ incrementanddecrement.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class incrementanddecrement{
+    public static void main(String args[]){
+        //post increment
+        int a=5;
+        System.out.println(a++);
+        //pre increment
+        int b=7;
+        System.out.println(++b);
+        //pre decrement
+        int c=5;
+        System.out.println(++c);
+        //post decrement
+        int d=6;
+        System.out.println(d++);
+    }
+}
