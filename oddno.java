ff4fa07efd8d3cddb9cdb839414fb3b26fd2d14e
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-05 13:29:05 +0530

    Add files via upload

---
+++ oddno.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class oddno{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        for(int i=1;i<=t;i+=2)
+        {
+            System.out.println(i);
+        }
+    }
+}
